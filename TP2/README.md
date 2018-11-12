b3a8f5eda480b68c1f27bb945d79597563a50f2c
alexandre282 <fontaniea@yahoo.fr>
2018-11-12 18:50:58 +0100

    Préparation TP2

---
+++ TP2/README.md
@@ -0,0 +1,31 @@
+# Asynchronous Server Technologies TP1
+## Alexandre Fontanié
+
+## Introduction
+Ce git contient le TP1 du cours Asynchronous Server Technologies, qui a pour but de nous familiariser avec les routes et queries.
+
+
+## Instructions
+
+### Pour lancer le serveur du TP1 :  
+Ouvrir la console dans le dossier du TP1 et utiliser la commande:
+```
+node index.js
+```
+
+### Les 3 routes de l'applications sont :  
+- [localhost:8080/](http://localhost:8080/)
+- [localhost:8080/hello?name=alexandre](http://localhost:8080/hello?name=alexandre)
+- [localhost:8080/hello?name=prenom](http://localhost:8080/hello?name=prenom")
+
+### Page home  
+La page http://localhost:8080/ racine décrit le fonctionnement du serveur.
+
+### Page Hello alexandre
+La page http://localhost:8080/hello?name=alexandre affiche une breve présentation de moi-même.
+
+### Page Hello
+La page http://localhost:8080/hello?name=prenom salut l'utilisateur par son prenom.
+
+### Autre page
+Toute autre page affiche une erreur 404 et un message "Page introuvable".
---
+++ TP2/handles.js
@@ -0,0 +1,30 @@
+// Import Node url and query module
+const url = require('url')
+const qs = require('querystring')
+
+module.exports = {
+  serverHandle: function (req, res) {
+    const route = url.parse(req.url)
+    const path = route.pathname
+    const params = qs.parse(route.query)
+
+    if (path === '/') {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write('Bonjour, vous etes a la racine du site internet. \n\n' +
+              'Allez sur http://localhost:8080/hello?name=alexandre pour en savoir plus sur moi \n\n' +
+              'Ou ecrivez votre prenom sur http://localhost:8080/hello?name=prenom pour un message surprise')
+    } else if (path === '/hello'  && 'name' in params && params.name === 'alexandre') {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write('Mon nom est Alexandre et je susi en 5eme annee a ECE Paris, ce TP correspond au cours Asynchronous Server Technologies')
+    } else if (path === '/hello'  && 'name' in params) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write('Bonjour ' + params['name'])
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write('Page introuvable')
+    }
+
+    res.end();
+
+  }
+}
