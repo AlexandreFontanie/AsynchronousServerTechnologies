ed009e3d50c98751b276748c7376dda5540a95c1
alexandre282 <fontaniea@yahoo.fr>
2018-11-12 12:56:15 +0100

    Params

--- TP1/test.js
+++ TP1/test.js
@@ -4,24 +4,19 @@ const http = require('http')
 const url = require('url')
 const qs = require('querystring')
 
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' +
-'    <body>' +
-'         <p>Hello World !2</p>' +
-'    </body>' +
-'</html>'
-
 const serverHandle = function (req, res) {
-  const path = url.parse(req.url).pathname;
-  console.log(path);
-  const queryParams = qs.parse(url.parse(req.url).query);
-  console.log(queryParams);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
+  const route = url.parse(req.url)
+  const path = route.pathname
+  const params = qs.parse(route.query)
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
+
   res.end();
 }
 
