ac6a3a9c57b1a7398b9da93741266aec7093610a
alexandre282 <fontaniea@yahoo.fr>
2018-12-16 23:35:25 +0100

    ajout des test

---
+++ TP3/src/metrics.test.ts
@@ -0,0 +1,71 @@
+import { expect } from 'chai';
+import { Metric, MetricsHandler } from './metrics';
+import { LevelDb } from "./leveldb";
+
+
+const dbPath: string = 'db_test/metrics';
+var dbMet: MetricsHandler;
+
+describe('Metrics', function () {
+  before(function () {
+    LevelDb.clear(dbPath)
+    dbMet = new MetricsHandler(dbPath)
+  })
+
+  after(function () {
+    dbMet.db.close()
+  })
+
+  describe('#get', function () {
+    it('should get empty array on non existing group', function (done) {
+      dbMet.get("user", "0", function (err: Error | null, result?: Metric[]) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.be.empty
+        done()
+      })
+    })
+  })
+
+  describe('#save', function () {
+    it('should save data', function (done) {
+      const met = [new Metric(`${new Date('2013-11-04 14:00 UTC').getTime()}`, 12)];
+      dbMet.save("user", "0", met, function (err: Error | null) {
+        done()
+      })
+    })
+
+    it('should not fail if data exist', function (done) {
+      dbMet.get("user", "0", function (err: Error | null, result?: Metric[]) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.be.an("array")
+        done()
+      })
+    })
+  })
+
+
+
+  describe('#delete', function () {
+    it('should delete data', function (done) {
+      const met = new Metric(`${new Date('2013-11-04 14:00 UTC').getTime()}`, 12);
+      dbMet.remove("user", "0", function (err: Error | null) {
+        expect(err).to.be.null
+        dbMet.get("user", "0", function (err: Error | null, result?: Metric[]) {
+          expect(err).to.be.null
+          expect(result).to.be.an("array")
+          expect(result).to.be.empty
+          done()
+        })
+      })
+    })
+
+    it('should not fail if data does not exist', function (done) {
+      dbMet.remove("user", "0", function (err: Error | null) {
+        expect(err).to.be.null
+        done()
+      })
+    })
+  })
+})
